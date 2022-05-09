75251c164f47f7b04b8106b0dfada0a28ecd0e62
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2022-05-09 11:45:34 +0200

    Added Kurser - Missing final styling and maybe data fetch

--- pages/kurser.tsx
+++ pages/kurser.tsx
@@ -1,6 +1,44 @@
 export default function Courses() {
   return (
-    <main>Jeg er kursus-siden</main>
+    <main>
+      <div className="container">
+        <h1>Kurser</h1>
+        <p>Kursusplan over MHs foreløbigt planlagte kurser. </p>
+        <p>Bemærk, at tilmelding sker direkte hos udbyderen og ikke her på siden!</p>
+        <h2>Forår 2022</h2>
+        <ul className="list-none">
+          <li>15.-16. januar - DOF Næstved</li>
+          <li>18.-19. januar - Symaskinetorvet i Slagelse. Tilmelding på 22787551</li>
+          <li>22.-23. januar - AOF Roskilde på Gundsømagle Gl. Skole</li>
+          <li>29.-30. januar - Nr. Snede Husflidsforening</li>
+          <li>2.-3. februar - Privat kursus på Als (lukket)</li>
+          <li>5.-6. februar - Broager v. Hanne Schmidt</li>
+          <li>12.-13. februar - AOF Holbæk</li>
+          <li>14.-15. februar - Nyborg Husflidsskole</li>
+          <li>26.-27. februar - Syværkstedet i Grenå v. Jannie Johanson</li>
+          <li>3.-4. marts - Det Kreative Hus, Skive</li>
+          <li>5.-6. marts - Susanne Axelsen, Skødstrup</li>
+          <li>8.-9. marts - AOF Roskilde på Gundsømagle Gl. Skole </li>
+          <li>12.-13. marts - AOF Roskilde på Gundsømagle Gl. Skole</li>
+          <li>24.-25. marts - Sabine Bruhn, Ringkøbing</li>
+          <li>23.-24. april - Broager v. Hanne Schmidt</li>
+          <li>26.-27. april - AOF Roskilde på Gundsømagle Gl. Skole</li>
+          <li>30. april-1.maj - AOF Svendborg</li>
+          <li>3.-4. maj - Nr. Kongerslev v. Heidi Nielsen</li>
+          <li>5.-6. maj - Hals v. Jette Jakobsen</li>
+          <li>14.-15. maj - Horne Aftenskole v. Henriette Nielsen</li>
+          <li>16.-17. maj - LOF Skive v/ Ann Theresa Bitsch</li>
+          <li>21.-22. maj - FOF Kolding/ Vejen</li>
+          <li>28.-29. maj - DOF Næstved Aftenskole</li>
+          <li>11.-12. juni - Nr. Snede Husflidsforening</li>
+          <li>13.-14. juni - LOF Skive v/ Ann Theresa Bitsch</li>
+          <li>18.-19. juni - Horne Aftenskole v. Henriette Nielsen</li>
+          <li>25.-26. juni - Grindløse/Bogense Husflidsforening</li>
+          <li>2.-3. juli - LOF Gribskov</li>
+          <li>7.-8. juli - Århus v. Jette Sloth Flohr</li>
+        </ul>
+      </div>
+    </main>
     //Oversigt over planlagte kurser (fra FB)
   );
 }
