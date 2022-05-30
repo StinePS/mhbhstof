4a69594bd661c4512948891301c40989a445660c
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2022-05-30 10:58:39 +0200

    Icons and images for site

---
+++ components/icons/IconMenu.tsx
@@ -0,0 +1,7 @@
+export default function IconMenu(props: React.ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg viewBox="0 0 48 48" fill="currentColor" {...props}>
+      <path d="m6 36v-3h36v3zm0-10.5v-3h36v3zm0-10.5v-3h36v3z" />
+    </svg>
+  );
+}
