5f55ea62bf8f8809288936d48d9b879aca554fc8
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2022-05-30 10:44:30 +0200

    changes to system setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const allowedImageWordPressDomain = new URL(
+  process.env.NEXT_PUBLIC_WORDPRESS_URL
+).hostname;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  // Specifying the allowed domains to ensure external urls can't be abused https://nextjs.org/docs/basic-features/image-optimization#domains
+  images: {
+    domains: [allowedImageWordPressDomain],
+  },
+  nextConfig,
+};
