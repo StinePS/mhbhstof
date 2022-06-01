1acfa362e3e332678927d678aae8668cc25e22f9
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2022-06-01 16:39:45 +0200

    Added address to Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -9,7 +9,7 @@ const FooterLink = ({
   return (
     <Link href={href}>
       <a
-        className="flex max-w-max items-center text-sm font-normal text-white no-underline hover:text-white hover:underline hover:underline-offset-2"
+        className="mb-2 flex max-w-max items-center text-sm font-normal text-white no-underline lg:hover:text-white lg:hover:underline lg:hover:underline-offset-2"
         {...props}
       />
     </Link>
@@ -19,7 +19,7 @@ const FooterLink = ({
 export default function Footer() {
   return (
     <footer className="text-sm">
-      <div className="my-auto flex w-full flex-row justify-evenly bg-purple p-4">
+      <div className="flex w-full flex-row justify-evenly bg-purple p-4">
         <FooterLink href="https://www.facebook.com/groups/144910305933685">
           <IconFB className="h-5 w-5" />
           Mh bh kurser
@@ -31,36 +31,26 @@ export default function Footer() {
         </FooterLink>
       </div>
 
-      <div className="w-full bg-darkpurple pb-4 lg:flex lg:justify-evenly">
-        <ul>
-          <li className="pl-4 pt-4 lg:pl-0">
-            <FooterLink href="/handelsbetingelser/#fragt">Fragt</FooterLink>
-          </li>
-          <li className="pl-4 pt-4 lg:pl-0">
-            <FooterLink href="/handelsbetingelser/#retur">Retur</FooterLink>
-          </li>
-        </ul>
-        <ul>
-          <li className="pl-4 pt-4 lg:pl-0">
-            <FooterLink href="/handelsbetingelser/#bestilling">
-              Bestilling
-            </FooterLink>
-          </li>
-
-          <li className="pl-4 pt-4 lg:pl-0">
-            <FooterLink href="/handelsbetingelser/#betaling">
-              Betaling
-            </FooterLink>
-          </li>
-        </ul>
-        <ul>
-          <li className="pl-4 pt-4 lg:pl-0">
-            <FooterLink href="/om">Om os</FooterLink>
-          </li>
-          <li className="pl-4 pt-4 lg:pl-0">
-            <FooterLink href="/om#kontakt">Kontakt</FooterLink>
-          </li>
-        </ul>
+      <div className="grid w-full grid-cols-2 bg-darkpurple p-4 lg:flex lg:justify-evenly">
+        <div>
+          <FooterLink href="/handelsbetingelser/#fragt">Fragt</FooterLink>
+          <FooterLink href="/handelsbetingelser/#retur">Retur</FooterLink>
+        </div>
+        <div>
+          <FooterLink href="/handelsbetingelser/#bestilling">
+            Bestilling
+          </FooterLink>
+          <FooterLink href="/handelsbetingelser/#betaling">Betaling</FooterLink>
+        </div>
+        <div>
+          <FooterLink href="/om">Om os</FooterLink>
+          <FooterLink href="/om#kontakt">Kontakt</FooterLink>
+        </div>
+        <div>
+          <address className="not-italic text-white">
+            Mh bhstof <br /> Kidnakken 11F <br /> 4930 Maribo
+          </address>
+        </div>
       </div>
     </footer>
   );
